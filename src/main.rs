23fc0ae08e55a480736cdc1ce987a41ddb5f8685
vimes <daniel.grayling.2020@gmail.com>
2022-05-15 13:48:57 +0100

    works

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-struct particle {
+struct Particle {
     x:i64,
     y:i64,
     z:i64
 }
 
 fn main() {
-    let mut vector: Vec<particle> = Vec::new();
-    vector.push(particle{x: 0, y: 0, z: 0});
-    vector.push(particle{x: 0, y: 0, z: 1});
+    let mut vector: Vec<Particle> = Vec::new();
+
+    let mut i = 0;
+    while i < 100 {
+        vector.push(Particle{x: 0, y: 0, z: 0});
+        i = i + 1;
+    }
 
     println!("Hello, world!");
 }
