081a6241eb773a8ecda09adbcf6c52b766bba68a
vimes <daniel.grayling.2020@gmail.com>
2022-06-12 20:20:00 +0100

    mutating

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use rand::Rng;
+ use rand::Rng; 
 
 struct Particle {
     x:f64,vx:f64,
     y:f64,vy:f64,
-    z:f64,vz:f64
+    z:f64,vz:f64,
+    mass: f64
 }
 
 fn express_mean_square_position(particles: &Vec<Particle>) {
@@ -34,12 +35,27 @@ fn express_mean_square_velocity(particles: &Vec<Particle>) {
     println!("Speeds {:?}", velocity);
 }
 
+fn time_step(mut particles: Vec<Particle>) -> Vec<Particle> {
+    for particle in particles.iter_mut() {
+        particle.x += particle.vx;
+        particle.y += particle.vy;
+        particle.z += particle.vz;
+    }
+
+    particles
+}
+
+//particles have a mailbox
+//all the other particles send their mass and position to the mailbox
+//particle calculates its new position and velocity
+//particle posts to central controller mailbox, iterate
+
 fn main() {
     let mut rng = rand::thread_rng();
     let mut particles: Vec<Particle> = Vec::new();
     let mut i = 0;
 
-    while i < 100 {
+    while i < 10 {
         particles.push(Particle{
             x: rng.gen_range(-10.0..10.0), 
             y: rng.gen_range(-10.0..10.0), 
@@ -47,20 +63,13 @@ fn main() {
             vx: rng.gen_range(-0.01..0.01), 
             vy: rng.gen_range(-0.01..0.01), 
             vz: rng.gen_range(-0.01..0.01),
+            mass: 1.0
         });
         i = i + 1;
     }
 
-    let mut distances: Vec<f64> = Vec::new();
+    let mut newParticles = time_step(particles);
 
-    for particle in particles.iter() {
-        let squared = particle.x * particle.x +
-        particle.y * particle.y +
-        particle.z * particle.z;
-
-        distances.push(squared.sqrt());
-    }
-
-    express_mean_square_position(&particles);
-    express_mean_square_velocity(&particles);
+    express_mean_square_position(&newParticles);
+    express_mean_square_velocity(&newParticles);
 }
